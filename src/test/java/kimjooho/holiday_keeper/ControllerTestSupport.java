78e909d89e8c30463b6332adbb177978988d222e
eora21 <eora21@naver.com>
2025-06-25 18:02:09 +0900

    test: 💍 컨트롤러 테스트 서포트 작성
    컨트롤러 테스트 동작 시 외부 api 동작하지 않도록 구성, MockMvc와 RestDocs 구성

---
+++ src/test/java/kimjooho/holiday_keeper/ControllerTestSupport.java
@@ -0,0 +1,25 @@
+package kimjooho.holiday_keeper;
+
+import kimjooho.holiday_keeper.holiday.service.HolidayService;
+import kimjooho.holiday_keeper.nager.NagerPreparer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+public class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockitoBean
+    protected NagerPreparer nagerPreparer;
+
+    @MockitoBean
+    protected HolidayService holidayService;
+}
