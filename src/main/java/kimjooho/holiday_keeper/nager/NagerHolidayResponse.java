df2f3fa85c71f8f0f1a0d449449af1128924f8a6
eora21 <eora21@naver.com>
2025-06-27 17:26:56 +0900

    fix: 🐛 nager의 행정구역, 타입이 null인 경우 빈 리스트 반환하도록 작성

--- src/main/java/kimjooho/holiday_keeper/nager/NagerHolidayResponse.java
+++ src/main/java/kimjooho/holiday_keeper/nager/NagerHolidayResponse.java
@@ -2,6 +2,7 @@ package kimjooho.holiday_keeper.nager;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import kimjooho.holiday_keeper.type.Type;
 
 public record NagerHolidayResponse(
@@ -9,6 +10,23 @@ public record NagerHolidayResponse(
         String name,
         String localName,
         List<String> counties,
-        List<Type> types
-        ) {
+        List<Type> types) {
+
+    @Override
+    public List<String> counties() {
+        if (Objects.isNull(counties)) {
+            return List.of();
+        }
+
+        return counties;
+    }
+
+    @Override
+    public List<Type> types() {
+        if (Objects.isNull(types)) {
+            return List.of();
+        }
+
+        return types;
+    }
 }
